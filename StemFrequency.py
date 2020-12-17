3403b03411fc697b7ae2a9e08b0951f25310bffe
David Cook <djc6@alum.lehigh.edu>
2020-12-17 05:00:20 -0500

    Update StemFrequency to be a class for better maintainability

--- StemFrequency.py
+++ StemFrequency.py
@@ -5,102 +5,102 @@ import sys
 from PorterStemmer import PorterStemmer
 
 
-TOKENIZER_PATTERN = r'\w+(?:[-\']\w+)*'
+class StemFrequency:
+    TOKENIZER_PATTERN = r'\w+(?:[-\']\w+)*'
 
+    def __init__(self, stemmer=None, stopwords_file='stopwords.txt', stopwords=None):
+        self.stemmer = stemmer if stemmer is not None else PorterStemmer()
+        self.stopwords = stopwords
+        if stopwords is None:
+            with open(stopwords_file, 'r') as f:
+                raw = f.read()
+            self.stopwords = re.findall(StemFrequency.TOKENIZER_PATTERN, raw)
 
-def tokenize_text(text):
-    """
-    Turns a string of text into a list of words.
-    Punctuation, numbers, and special characters are removed, but words may still contain apostrophes.
-    Removes hyphens after tokenizing, so that multiple hyphens define word boundaries, but single hyphens do not.
-    :param text: the text to tokenize
-    :return: a list of words
-    """
-    tokens = re.findall(TOKENIZER_PATTERN, re.sub(r'[0-9_]+', '', text.lower()))
-    return list(map(lambda x: x.replace('-', ''), tokens))
+    def tokenize_text(self, text):
+        """
+        Turns a string of text into a list of words.
+        Punctuation, numbers, and special characters are removed, but words may still contain apostrophes.
+        Removes hyphens after tokenizing, so that multiple hyphens define word boundaries, but single hyphens do not.
+        :param text: the text to tokenize
+        :return: a list of words
+        """
+        tokens = re.findall(StemFrequency.TOKENIZER_PATTERN, re.sub(r'[0-9_]+', '', text.lower()))
+        return list(map(lambda x: x.replace('-', ''), tokens))
 
+    def find_frequency(self, words):
+        """
+        Takes a list of words and makes a dictionary mapping unique words to their frequency in the original list
+        :param words: a list of words
+        :return: a dictionary where the keys are words, and the values are frequencies
+        """
+        frequency = {}
+        for word in words:
+            if word not in frequency:
+                frequency[word] = 1
+            else:
+                frequency[word] += 1
+        return frequency
 
-def find_frequency(words):
-    """
-    Takes a list of words and makes a dictionary mapping unique words to their frequency in the original list
-    :param words: a list of words
-    :return: a dictionary where the keys are words, and the values are frequencies
-    """
-    frequency = {}
-    for word in words:
-        if word not in frequency:
-            frequency[word] = 1
-        else:
-            frequency[word] += 1
-    return frequency
+    def remove_stopwords(self, words):
+        """
+        Removes blacklisted keys from a dictionary
+        :param words: A dictionary where the keys are to be compared against a blacklist
+        :param stopwords: A list of keys to remove from a dictionary
+        :return: a dictionary with the blacklisted keys removed
+        """
+        for stopword in self.stopwords:
+            if stopword in words:
+                words.pop(stopword)
+        return words
 
+    def stem_and_consolidate(self, frequencies, remove_apostrophes=False):
+        """
+        Takes a word-to-frequency dictionary and stems the words, summing the frequencies of words that map to the same stem
+        Also, apostrophes are removed first.
+        :param frequencies: a dictionary of words to frequencies
+        :param remove_apostrophes: if True, then remove the apostrophes from words before stemming
+        :return: a dictionary of stems to frequencies
+        """
+        consolidated = {}
+        for word, frequency in frequencies.items():
+            if remove_apostrophes:
+                word = word.replace("'", "")
+            stemmed = self.stemmer.stem(word, 0, len(word) - 1)
+            if stemmed in consolidated:
+                consolidated[stemmed] += frequency
+            else:
+                consolidated[stemmed] = frequency
+        return consolidated
 
-def remove_stopwords(words, stopwords):
-    """
-    Removes blacklisted keys from a dictionary
-    :param words: A dictionary where the keys are to be compared against a blacklist
-    :param stopwords: A list of keys to remove from a dictionary
-    :return: a dictionary with the blacklisted keys removed
-    """
-    for stopword in stopwords:
-        if stopword in words:
-            words.pop(stopword)
-    return words
+    def most_common(self, frequencies, count):
+        """
+        Given a dictionary of strings to their frequency, finds the most common strings in order of descending
+        frequency, then ascending alphabetical
+        :param frequencies: a dictionary of strings to their frequency
+        :param count: the number of strings to return
+        :return: a list of tuples of the most common strings in descending order of frequency: (stem, frequency)
+        """
+        return heapq.nsmallest(count, frequencies.items(), key=lambda x: (-x[1], x[0]))
 
-
-def stem_and_consolidate(frequencies, remove_apostrophes=False):
-    """
-    Takes a word-to-frequency dictionary and stems the words, summing the frequencies of words that map to the same stem
-    Also, apostrophes are removed first.
-    :param frequencies: a dictionary of words to frequencies
-    :param remove_apostrophes: if True, then remove the apostrophes from words before stemming
-    :return: a dictionary of stems to frequencies
-    """
-    stemmer = PorterStemmer()
-    consolidated = {}
-    for word, frequency in frequencies.items():
-        if remove_apostrophes:
-            word = word.replace("'", "")
-        stemmed = stemmer.stem(word, 0, len(word) - 1)
-        if stemmed in consolidated:
-            consolidated[stemmed] += frequency
-        else:
-            consolidated[stemmed] = frequency
-    return consolidated
-
-
-def most_common(frequencies, count):
-    """
-    Given a dictionary of strings to their frequency, finds the most common strings in order of descending
-    frequency, then ascending alphabetical
-    :param frequencies: a dictionary of strings to their frequency
-    :param count: the number of strings to return
-    :return: a list of tuples of the most common strings in descending order of frequency: (stem, frequency)
-    """
-    return heapq.nsmallest(count, frequencies.items(), key=lambda x: (-x[1], x[0]))
-
-
-def find_common_stems(file_name, count=20):
-    """
-    Finds the most common word stems in a text file, excluding certain stopwords
-    :param file_name: the name of the text file to find the common word stems in
-    :param count: the number of stems to find
-    :return: a list of the most common stems in the file, in descending order of frequency
-    """
-    with open(file_name, 'r') as f:
-        raw = f.read()
-    words = tokenize_text(raw)
-    word_counts = find_frequency(words)
-    with open('stopwords.txt', 'r') as f:
-        raw = f.read()
-    stopwords = re.findall(TOKENIZER_PATTERN, raw)
-    cleaned_counts = remove_stopwords(word_counts, stopwords)
-    stem_counts = stem_and_consolidate(cleaned_counts, remove_apostrophes=True)
-    return most_common(stem_counts, count)
+    def find_common_stems(self, file_name, count=20):
+        """
+        Finds the most common word stems in a text file, excluding certain stopwords
+        :param file_name: the name of the text file to find the common word stems in
+        :param count: the number of stems to find
+        :return: a list of the most common stems in the file, in descending order of frequency
+        """
+        with open(file_name, 'r') as f:
+            raw = f.read()
+        words = self.tokenize_text(raw)
+        word_counts = self.find_frequency(words)
+        cleaned_counts = self.remove_stopwords(word_counts)
+        stem_counts = self.stem_and_consolidate(cleaned_counts, remove_apostrophes=True)
+        return self.most_common(stem_counts, count)
 
 
 if __name__ == '__main__':
     # get filepath if passed-in
-    filepath = sys.argv[1] if len(sys.argv) > 1 else 'Text1.txt'
-    for stem in find_common_stems(filepath, 20):
+    filepath = sys.argv[1] if len(sys.argv) > 1 else 'Text2.txt'
+    stem_frequency = StemFrequency()
+    for stem in stem_frequency.find_common_stems(filepath, 20):
         print("{} ({})".format(stem[0], stem[1]))
--- TestStemFrequency.py
+++ TestStemFrequency.py
@@ -1,79 +1,84 @@
 import unittest
-from StemFrequency import tokenize_text, find_frequency, remove_stopwords, stem_and_consolidate, most_common
+from StemFrequency import StemFrequency
 
 
 class TestStemFrequency(unittest.TestCase):
     def test_tokenize_text(self):
         check = TestStemFrequency._assert_msg
+        sf = StemFrequency()
         text = "a ab a-b aa-bb aa'bb --ab ab-- 'word word' \"word word\" ab\nab\tab 1 1234567890 \\ "\
                "end. end! end? * $ % # a\u0144b 1ab ab1 a1b dash--dash abc's out-of-the-way "\
                " _a_ b_c d__e __f__ _g h_"
         expected = ["a", "ab", "ab", "aabb", "aa'bb", "ab", "ab", "word", "word", "word", "word", "ab", "ab", "ab",
                     "end", "end", "end", "a\u0144b", "ab", "ab", "ab", "dash", "dash", "abc's", "outoftheway",
                     "a", "bc", "de", "f", "g", "h"]
-        check(tokenize_text(text), expected)
+        check(sf.tokenize_text(text), expected)
         text = "ours\n\tourselves\nout\nover\nown\nsame\nshan't\nshe\nshe'd\nshe'll"
         expected = ["ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll"]
-        check(tokenize_text(text), expected)
-        check(tokenize_text(""), [])
+        check(sf.tokenize_text(text), expected)
+        check(sf.tokenize_text(""), [])
 
     def test_find_frequency(self):
         check = TestStemFrequency._assert_msg
+        sf = StemFrequency()
         words = ["a", "b", "a", "c", "ab", "aa", "a-b", "a'b", "ab", "ab", "a-b", "a'b"]
         expected = {"a": 2, "b": 1, "c": 1, "ab": 3, "aa": 1, "a-b": 2, "a'b": 2}
-        check(find_frequency(words), expected)
-        check(find_frequency([]), {})
+        check(sf.find_frequency(words), expected)
+        check(sf.find_frequency([]), {})
 
     def test_remove_stopwords(self):
         check = TestStemFrequency._assert_msg
         stopwords = ["a", "b", "ab", "xyz", "ab-cd", "aa'b"]
-        check(remove_stopwords({}, []), {})
-        check(remove_stopwords({}, stopwords), {})
-        check(remove_stopwords({"c": 1, "cd": 2}, stopwords), {"c": 1, "cd": 2})
-
+        sf = StemFrequency(stopwords=stopwords)
         words = {"c": 1, "a": 2, "abc": 3, "xy": 4, "wxyz": 5, "ab-cd": 6, "aa'b": 7, "xyz": 8}
         expected = {"c": 1, "abc": 3, "xy": 4, "wxyz": 5}
-        check(remove_stopwords(words, stopwords), expected)
+        check(sf.remove_stopwords(words), expected)
+        check(sf.remove_stopwords({}), {})
+        check(sf.remove_stopwords({"c": 1, "cd": 2}), {"c": 1, "cd": 2})
+
+
 
     def test_stem_and_consolidate(self):
         check = TestStemFrequency._assert_msg
-        check(stem_and_consolidate({"a": 5}), {"a": 5})
-        check(stem_and_consolidate({}), {})
-        check(stem_and_consolidate({"a": 5}), {"a": 5})
-        check(stem_and_consolidate({"nature's": 5}), {"nature'": 5})
-        check(stem_and_consolidate({"nature's": 5}, remove_apostrophes=True), {"natur": 5})
-        check(stem_and_consolidate({"natures": 5}), {"natur": 5})
-        check(stem_and_consolidate({"nature": 5}), {"natur": 5})
-        check(stem_and_consolidate({"can't": 5}), {"can't": 5})
-        check(stem_and_consolidate({"can't": 5}, remove_apostrophes=True), {"cant": 5})
-        check(stem_and_consolidate({"cant": 5}), {"cant": 5})
-        check(stem_and_consolidate({"dinah'll": 5}), {"dinah'l": 5})
-        check(stem_and_consolidate({"dinah'll": 5}, remove_apostrophes=True), {"dinahl": 5})
-        check(stem_and_consolidate({"dinahll": 5}), {"dinahl": 5})
-        check(stem_and_consolidate({"dinah": 5}), {"dinah": 5})
-        check(stem_and_consolidate({"to-day": 5}), {"to-dai": 5})
-        check(stem_and_consolidate({"today": 5}), {"todai": 5})
-        check(stem_and_consolidate({"self-evident": 5}), {"self-evid": 5})
-        check(stem_and_consolidate({"selfevident": 5}), {"selfevid": 5})
-        check(stem_and_consolidate({"dogs": 5}), {"dog": 5})
-        check(stem_and_consolidate({"blindly": 5}), {"blindli": 5})
-        check(stem_and_consolidate({"running": 5}), {"run": 5})
-        check(stem_and_consolidate({"dog": 3, "a": 2, "dogs": 4}), {"dog": 7, "a": 2})
-        check(stem_and_consolidate({"dog": 3, "blind": 2, "dogs": 4, "blinds": 5, "blinding": 6}),
+        sf = StemFrequency()
+        check(sf.stem_and_consolidate({"a": 5}), {"a": 5})
+        check(sf.stem_and_consolidate({}), {})
+        check(sf.stem_and_consolidate({"a": 5}), {"a": 5})
+        check(sf.stem_and_consolidate({"nature's": 5}), {"nature'": 5})
+        check(sf.stem_and_consolidate({"nature's": 5}, remove_apostrophes=True), {"natur": 5})
+        check(sf.stem_and_consolidate({"natures": 5}), {"natur": 5})
+        check(sf.stem_and_consolidate({"nature": 5}), {"natur": 5})
+        check(sf.stem_and_consolidate({"can't": 5}), {"can't": 5})
+        check(sf.stem_and_consolidate({"can't": 5}, remove_apostrophes=True), {"cant": 5})
+        check(sf.stem_and_consolidate({"cant": 5}), {"cant": 5})
+        check(sf.stem_and_consolidate({"dinah'll": 5}), {"dinah'l": 5})
+        check(sf.stem_and_consolidate({"dinah'll": 5}, remove_apostrophes=True), {"dinahl": 5})
+        check(sf.stem_and_consolidate({"dinahll": 5}), {"dinahl": 5})
+        check(sf.stem_and_consolidate({"dinah": 5}), {"dinah": 5})
+        check(sf.stem_and_consolidate({"to-day": 5}), {"to-dai": 5})
+        check(sf.stem_and_consolidate({"today": 5}), {"todai": 5})
+        check(sf.stem_and_consolidate({"self-evident": 5}), {"self-evid": 5})
+        check(sf.stem_and_consolidate({"selfevident": 5}), {"selfevid": 5})
+        check(sf.stem_and_consolidate({"dogs": 5}), {"dog": 5})
+        check(sf.stem_and_consolidate({"blindly": 5}), {"blindli": 5})
+        check(sf.stem_and_consolidate({"running": 5}), {"run": 5})
+        check(sf.stem_and_consolidate({"dog": 3, "a": 2, "dogs": 4}), {"dog": 7, "a": 2})
+        check(sf.stem_and_consolidate({"dog": 3, "blind": 2, "dogs": 4, "blinds": 5, "blinding": 6}),
               {"dog": 7, "blind": 13})
 
     def test_most_common(self):
         freq = {"c": 1, "b": 2, "a": 1, "d": 3, "e": 1, "f": 5, "g": 3}
         check = TestStemFrequency._assert_msg
-        check(most_common({}, 0), [])
-        check(most_common(freq, 0), [])
-        check(most_common(freq, 1), [("f", 5)])
-        check(most_common(freq, 2), [("f", 5), ("d", 3)])
-        check(most_common(freq, 3), [("f", 5), ("d", 3), ("g", 3)])
-        check(most_common(freq, 4), [("f", 5), ("d", 3), ("g", 3), ("b", 2)])
-        check(most_common(freq, 5), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1)])
-        check(most_common(freq, 8), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1), ("c", 1), ("e", 1)])
-        check(most_common(freq, 100), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1), ("c", 1), ("e", 1)])
+        sf = StemFrequency()
+        check(sf.most_common({}, 0), [])
+        check(sf.most_common(freq, 0), [])
+        check(sf.most_common(freq, 1), [("f", 5)])
+        check(sf.most_common(freq, 2), [("f", 5), ("d", 3)])
+        check(sf.most_common(freq, 3), [("f", 5), ("d", 3), ("g", 3)])
+        check(sf.most_common(freq, 4), [("f", 5), ("d", 3), ("g", 3), ("b", 2)])
+        check(sf.most_common(freq, 5), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1)])
+        check(sf.most_common(freq, 8), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1), ("c", 1), ("e", 1)])
+        check(sf.most_common(freq, 100), [("f", 5), ("d", 3), ("g", 3), ("b", 2), ("a", 1), ("c", 1), ("e", 1)])
 
     @staticmethod
     def _assert_msg(actual, expected):
